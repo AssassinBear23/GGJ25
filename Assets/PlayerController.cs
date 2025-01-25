c58b9b8fc01a63d50baa082d3ab3a4cce431ff5b
Wes te Braak <wesbraak3@hotmail.com>
2025-01-25 17:51:35 +0100

    buble fix

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,24 +5,45 @@ using UnityEngine.U2D;
 public class PlayerController : MonoBehaviour {
     public float moveSpeed = 5f;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D playerBody;
     private float moveInput;
 
+    public bool inBubbleCollumn = false;
+    [SerializeField] private float CurretnBubbleSize;
+    [SerializeField] private float growSpeed = 0.2f;
+    [SerializeField] private float shrinkSpeed = 0.2f;
+    [SerializeField] private float minSize = 0.5f;
+    [SerializeField] private float maxSize = 5.0f;
+
+    [SerializeField] private float returnBias = 0.1f;
+    [SerializeField] private Vector2[] originalPoints;
+
+    #region Visual Variables
     private const float splineOffset = 0.5f;
 
-    [SerializeField]
-    private SpriteShapeController spriteShape;
-    [SerializeField]
-    private Transform[] points;
-
+    [SerializeField] private SpriteShapeController spriteShape;
+    [SerializeField] private Transform[] points;
+    #endregion
 
     private void Awake() {
+        LogOriginalPoints();
         UpdateVerticies();
     }
 
+    /// <summary>
+    /// Logs the original points of the player.
+    /// </summary>
+    private void LogOriginalPoints() {
+        int childCount = transform.childCount;
+        originalPoints = new Vector2[childCount];
+
+        for (int i = 0; i < childCount; i++) {
+            originalPoints[i] = transform.GetChild(i).localPosition;
+        }
+    }
 
     void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
@@ -31,13 +52,44 @@ public class PlayerController : MonoBehaviour {
         moveInput = Input.GetAxis("Horizontal");
 
         if (Input.GetKey(KeyCode.S)) {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, -moveSpeed);
+            foreach (Transform child in transform) {
+                SpringJoint2D[] springs = child.GetComponents<SpringJoint2D>();
+
+                foreach (SpringJoint2D spring in springs) {
+                    if (CurretnBubbleSize > minSize) {
+                        spring.distance -= shrinkSpeed * Time.deltaTime;
+                        CurretnBubbleSize -= shrinkSpeed * Time.deltaTime;
+                    }
+                }
+            }
+        }
+
+        if (inBubbleCollumn && Input.GetKey(KeyCode.W)) {
+            foreach (Transform child in transform) {
+                SpringJoint2D[] springs = child.GetComponents<SpringJoint2D>();
+
+                foreach (SpringJoint2D spring in springs) {
+                    if (CurretnBubbleSize < maxSize) {
+                        spring.distance += growSpeed * Time.deltaTime;
+                        CurretnBubbleSize += growSpeed * Time.deltaTime;
+                    }
+                }
+            }
         }
     }
 
     void FixedUpdate() {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        playerBody.linearVelocity = new Vector2(moveInput * moveSpeed, playerBody.linearVelocity.y);
+
+        foreach (Transform child in transform) {
+            Vector2 directionToOriginal = (originalPoints[child.GetSiblingIndex()] - (Vector2)child.localPosition).normalized;
+            child.localPosition = Vector2.Lerp(child.localPosition, originalPoints[child.GetSiblingIndex()], returnBias * Time.fixedDeltaTime);
+        }
     }
+
+    /// <summary>
+    /// Updates the verticies of the sprite shape to match the points of the player.
+    /// </summary>
     private void UpdateVerticies() {
         for (int i = 0; i < points.Length - 1; i++) {
             Vector2 _vertex = points[i].localPosition;
@@ -46,7 +98,8 @@ public class PlayerController : MonoBehaviour {
             float _colliderRadius = points[i].gameObject.GetComponent<CircleCollider2D>().radius;
             try {
                 spriteShape.spline.SetPosition(i, _vertex - _towardsCenter * _colliderRadius);
-            } catch {
+            }
+            catch {
                 Debug.Log("Error: " + i);
                 spriteShape.spline.SetPosition(i, _vertex - _towardsCenter * (_colliderRadius + splineOffset));
             }
